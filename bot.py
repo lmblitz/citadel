74e024a3b15b6131ab06aede4c4c65a5013918b3
Cash Jundi <notmoodsfr@gmail.com>
2026-07-31 10:52:48 -0500

    ..

--- bot.py
+++ bot.py
@@ -7,10 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.default())
 
 
 async def setup_hook():
